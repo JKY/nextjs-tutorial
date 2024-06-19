a4c001a83730175f0cdb3b317f68f934cee0f717
Jacky.Y <yanghf1985@hotmail.com>
2024-06-19 09:05:40 +0800

    update

--- app/api/route.js
+++ app/api/route.js
@@ -1,9 +1,4 @@
 import data from "@/app/data/questions"
-import { MongoClient } from "mongodb";
-if (!global.client)
-    global.client = new MongoClient(`mongodb://localhost:27017`);
 export async function GET(request) {
-    const items = await global.client.db("foo").collection("foo").find({}).toArray();
-    console.log(items);
     return Response.json({});
 }
